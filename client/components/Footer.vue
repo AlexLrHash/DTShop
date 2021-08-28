eb8eb9e5c5f049c7100c3a0322bc4b75a40f0306
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2021-08-28 23:39:28 +0300

    auth pages and cart

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="text-muted">
+  <footer class="text-muted mt-3">
 
     <div class="container">
       <p class="float-right">
-        <a href="#">Back to top</a>
+        <a href="#">Наверх</a>
       </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+      <p>Взято из &copy; Bootstrap!</p>
+      <p>Сайт был написан группой разработчиков.</p>
     </div>
 
   </footer>
