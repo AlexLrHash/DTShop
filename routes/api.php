0e8f84032993063eb20c0197c4716fe91c7b770f
acespan <87124399+acespan@users.noreply.github.com>
2021-08-29 22:04:51 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+=======
+Route::get('/user/cards', [\App\Http\Controllers\CardController::class, 'index'])->name('user.cards.index');
+Route::post('/user/cards', [\App\Http\Controllers\CardController::class , 'store'])->name('user.cards.store');
+Route::get('/user/cards/{id}', [\App\Http\Controllers\CardController::class, 'show'])->name('user.cards.show');
+Route::put('/user/cards/{id}', [\App\Http\Controllers\CardController::class, 'update'])->name('user.cards.update');
+Route::delete('/user/cards/{id}', [\App\Http\Controllers\CardController::class, 'destroy'])->name('user.cards.destroy');
 =======
 Route::get('/basket/{basket}',[\App\Http\Controllers\BasketController::class, 'show'])->name('basket.show');
 Route::post('/basket/{basket}',[\App\Http\Controllers\BasketController::class, 'add'])->name('basket.add');
 Route::delete('/basket/{basket_id}',[\App\Http\Controllers\BasketController::class, 'destroy'])->name('basket.destroy');
 Route::get('/basket',[\App\Http\Controllers\BasketController::class, 'index'])->name('basket.index');
-
+=======
 Route::get('dota/products', [\App\Http\Controllers\DotaProductController::class, 'index'])->name('dota.index');
 Route::get('dota/products/{products}', [\App\Http\Controllers\DotaProductController::class, 'show'])->name('dota.show');
 Route::post('dota/products', [\App\Http\Controllers\DotaProductController::class, 'store'])->name('dota.store');
