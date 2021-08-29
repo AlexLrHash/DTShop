c2bff49d2ab6dfe5b802ae2f1448330f0b190a95
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2021-08-29 10:25:53 +0300

    admin Page

---
+++ client/pages/admincarts.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <AdminHeader></AdminHeader>
+    <AdminCarts></AdminCarts>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admincarts"
+}
+</script>
+
+<style scoped>
+
+</style>
